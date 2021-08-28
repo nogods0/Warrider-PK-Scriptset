04f0fbc2b4ab5fa598454b46f2a5af26393a563e
nogods0 <42708223+nogods0@users.noreply.github.com>
2021-08-28 15:05:31 +0800

    Update warrider_database.sql
    CREATE TABLE `whitelist` (
      `id` int(10) UNSIGNED NOT NULL,
      `guid` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
      `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
    )
    
    CREATE TABLE `whitelist` (
      `id` int(10) UNSIGNED NOT NULL,
      `guid` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
      `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
    )

--- Warrider Scriptset/warrider_database.sql
+++ Warrider Scriptset/warrider_database.sql
@@ -45,7 +45,7 @@ CREATE TABLE `ban_list` (
 CREATE TABLE `whitelist` (
   `id` int(10) UNSIGNED NOT NULL,
   `guid` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
